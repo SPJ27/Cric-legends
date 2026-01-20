52ebcb74014c73eeb27df7b83c218579df063d10
Saksham Jain <saksham.khatod27@gmail.com>
2026-01-20 06:56:54 +0530

    Added requirements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import pdfplumber
 from Read_scorecard import get_object
 from MVP import calculate_mvp
 from Awards import determine_awards
 
-
+print(pdfplumber.__version__)
 
 players_stats = get_object("I:\\Cric-legends\\uploads\\A_vs_B_1760858094289.pdf")
 for player in players_stats:
